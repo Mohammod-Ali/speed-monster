1d66f12b06bdeda0e876945b5d30ed332da424ed
Mohammod Ali <mohammodali651@gmail.com>
2022-09-15 21:21:16 +0600

    blogs and history style added

--- script.js
+++ script.js
@@ -57,8 +57,10 @@ const typeController = (e) => {
 const validate = (key) => {
   if (key === questionText[userText.length - 1]) {
     return true;
+  }else{
+    return false;
   }
-  return false;
+  
 };
 
 // FINISHED TYPING
